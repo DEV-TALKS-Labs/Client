30848436810672628aa617b622854843e3e752be
Emad Anwer <emadanwer.official@gmail.com>
2023-12-08 09:57:27 +0200

    fix MultiSelect bg color when making newRoom

--- src/components/MultiSelect.jsx
+++ src/components/MultiSelect.jsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { useState } from "react";
 
-const MultiSelect = ({ options, numberOfVisibles }) => {
+const MultiSelect = ({ options, numberOfVisibles, creatNewRoomMode }) => {
   const [selectedItems, setSelectedItems] = useState([]);
   const [showAllOptions, setShowAllOptions] = useState(false);
 
   const initiallyVisibleOptions = numberOfVisibles || 9;
+  const uiModeCreatNewRoom = creatNewRoomMode || false;
   const handleSelectionChange = (value) => {
     if (selectedItems.includes(value)) {
       setSelectedItems(selectedItems.filter((item) => item !== value));
@@ -18,15 +19,15 @@ const MultiSelect = ({ options, numberOfVisibles }) => {
     : options.slice(0, initiallyVisibleOptions);
 
   return (
-    <div className="pr-4 py-4 flex w-full">
-      <ul className="flex flex-wrap max-w-full">
+    <div className='pr-4 py-4 flex w-full'>
+      <ul className='flex flex-wrap max-w-full'>
         {options.length > initiallyVisibleOptions && (
-          <li className="mb-2 mr-3 shadow-lg border border-blue-500 rounded-md">
-            <label className="flex items-center">
+          <li className='mb-2 mr-3 shadow-lg border border-blue-500 rounded-md'>
+            <label className='flex items-center'>
               <input
-                type="checkbox"
+                type='checkbox'
                 onChange={() => setShowAllOptions(!showAllOptions)}
-                className="hidden"
+                className='hidden'
               />
               <span
                 className={`cursor-pointer p-3 rounded-md ${
@@ -43,21 +44,21 @@ const MultiSelect = ({ options, numberOfVisibles }) => {
         {visibleOptions.map((option) => (
           <li
             key={option.id}
-            className="mb-2 mr-3 shadow-lg border border-blue-500 rounded-md"
+            className='mb-2 mr-3 shadow-lg border border-blue-500 rounded-md'
           >
-            <label className="flex items-center">
+            <label className='flex items-center'>
               <input
-                type="checkbox"
+                type='checkbox'
                 value={option.name}
                 checked={selectedItems.includes(option)}
                 onChange={() => handleSelectionChange(option)}
-                className="hidden"
+                className='hidden'
               />
               <span
                 className={`cursor-pointer p-3 rounded-md ${
                   selectedItems.includes(option)
                     ? "text-white bg-blue-500"
-                    : "text-gray-700"
+                    : uiModeCreatNewRoom ? "bg-white text-gray-700" : "text-gray-700"
                 }`}
               >
                 {option.name}
